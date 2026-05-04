6b30b72722c38892469b8f81cbc4a27a37e2075b
knacko <geekbike@hotmail.com>
2026-05-04 14:31:08 -0600

    build: update version #

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='search_tools',
-    version='0.1.0-alpha',
+    version='1.0.0',
     packages=find_packages(exclude=['tests*']),
     install_requires=[
         'alive_progress',
@@ -13,7 +13,7 @@ setup(
     ],
     python_requires='>=3.10, <4',
     description='A library of search functions for various projects at APL.',
-    url='https://github.com/provlab-bioinfo/search_tools',
+    url='https://github.com/provlab-bioinfo/search-tools',
     author='Andrew Lindsay',
     author_email='andrew.lindsay@albertaprecisionlabs.ca',
     include_package_data=True,
